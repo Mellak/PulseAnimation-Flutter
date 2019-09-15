3b245f4fad22df7dd2c7569f42d1210298437f14
UnessM <40334681+UnessM@users.noreply.github.com>
2019-09-15 20:56:31 +0100

    Pusle Animation - Flutter

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: 'Flutter Annimation'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  AnimationController motionController;
+  Animation motionAnimation;
+  double size = 20;
+  void initState() {
+    super.initState();
+
+    motionController = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+      lowerBound: 0.5,
+    );
+
+    motionAnimation = CurvedAnimation(
+      parent: motionController,
+      curve: Curves.ease,
+    );
+
+    motionController.forward();
+    motionController.addStatusListener((status) {
+      setState(() {
+        if (status == AnimationStatus.completed) {
+          motionController.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          motionController.forward();
+        }
+      });
+    });
+
+    motionController.addListener(() {
+      setState(() {
+        size = motionController.value * 250;
+      });
+    });
+    // motionController.repeat();
+  }
+
+  @override
+  void dispose() {
+    motionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //A bonus For you
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 200, left: 8, right: 8),
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Container(
+                child: Stack(children: <Widget>[
+                  Center(
+                    child: new Container(
+                      child: Image.asset('images/trg.png'),
+                      height: size,
+                    ),
+                  ),
+                ]),
+                height: 200,
+              ),
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
